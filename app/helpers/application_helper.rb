2172915b5802d72ec713ec802c5a362371bc7b90
Fela Winkelmolen <felawinkelmolen@gmail.com>
2012-11-06 12:20:33 +0100

    fix glyph regression

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,5 @@
 module ApplicationHelper
   def glyph(*names)
-    content_tag :i, nil, class: names.map do |name|
-      "icon-#{name.to_s.gsub('_','-')}"
-    end
+    content_tag :i, nil, class: names.map{|name| "icon-#{name.to_s.gsub('_','-')}" }
   end
 end
