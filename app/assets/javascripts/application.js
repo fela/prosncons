575a941916cfdf54bff7a7525801fa086e2b496f
Fela Winkelmolen <felawinkelmolen@gmail.com>
2013-07-16 23:18:38 +0200

    voting authentication ui

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,6 +29,8 @@ function vote(action, id, diff) {
 }
 
 $(document).ready(function(){
+    // make whole table rows links
+    // TODO only do if they follow a certain class
     $("tr").click(function(){
         var newLocation = $(this).attr('url');
         if (newLocation)
@@ -37,5 +39,31 @@ $(document).ready(function(){
         $(this).addClass('hovered');
     }).mouseout(function(){
         $(this).removeClass('hovered');
+    });
+
+    // votes
+    $('.vote-up').click(function() {
+        var votes = $(this).parent('.votes');
+        if (votes.hasClass('up-voted')) {
+            // undo vote
+            votes.removeClass('up-voted');
+        } else {
+            // the class either has no vote or a down vote
+            // in either case vote up
+            votes.removeClass('down-voted'); // might do nothing
+            votes.addClass('up-voted');
+        }
+    });
+    // analogous as an up-vote
+    $('.vote-down').click(function() {
+        var votes = $(this).parent('.votes');
+        if (votes.hasClass('down-voted')) {
+            // undo vote
+            votes.removeClass('down-voted');
+        }
+        else {
+            votes.removeClass('up-voted');
+            votes.addClass('down-voted');
+        }
     });
 });
