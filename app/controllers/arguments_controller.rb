90d38a46ae594a2a62568c4f6504d632be7b82ce
Fela Winkelmolen <felawinkelmolen@gmail.com>
2013-07-19 19:42:52 +0200

    refactoring
    removed some unneeded DRYness

--- app/controllers/arguments_controller.rb
+++ app/controllers/arguments_controller.rb
@@ -1,15 +1,16 @@
 class ArgumentsController < ApplicationController
-  before_filter :set_page_and_option
 
   def new
     @argument = Argument.new()
     @page = current_page
     @argument.page = @page
+    @option = params[:option]
     @argument.option = @option
   end
 
   def create
     @page = current_page
+    @option = params[:option]
     @argument = Argument.new(params[:argument])
     @argument.option = @option
     @argument.page = @page
@@ -32,18 +33,13 @@ class ArgumentsController < ApplicationController
 
     if @argument.update_attributes(params[:argument])
       flash[:success] = 'Argument was successfully updated'
-      redirect_to @page
+      redirect_to current_page
     else
       render action: "edit"
     end
   end
 
 private
-  def set_page_and_option
-    @page = current_page
-    @option = params[:option]
-  end
-
   def current_page
     Page.find(params[:page_id])
   end
